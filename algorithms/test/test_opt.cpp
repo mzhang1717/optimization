54f2ca68b6004269646e4766491f029a28791556
Mingfeng Zhang <jason.mfzhang@gmail.com>
2026-02-15 11:49:49 -0800

    test 3d convex function

--- algorithms/test/test_opt.cpp
+++ algorithms/test/test_opt.cpp
@@ -9,21 +9,59 @@
 #include "../../objectivefunction/costfunctionmccormick.h"
 #include "../../objectivefunction/costfunctionquadratic.h"
 #include "../../objectivefunction/costfunctionrosenbrock.h"
+#include "../../objectivefunction/costfunctionconvex3d.h"
 //#include "costfunctionquadratic.h"
 
 int main () {
+
+    // ----- CostFunctionConvex3D (3D: f(x) = x1^2 + x2^2 + x3^2, minimum at (0,0,0)) -----
+    std::cout << "======= CostFunctionConvex3D =======" << std::endl;
+    CostFunctionConvex3D convex3d_function;
+    std::unique_ptr<OptimizerBase> ptrOptimizer;
+
+    std::cout << "------- Newton method -----" << std::endl;
+    ptrOptimizer.reset(new OptimizerNewton(convex3d_function, Eigen::Vector3d{1.0, 1.0, 1.0}));
+    ptrOptimizer->optimize();
+    ptrOptimizer->setInitialGuess(Eigen::Vector3d{-2.0, 3.0, 1.0});
+    ptrOptimizer->optimize();
+    ptrOptimizer->setInitialGuess(Eigen::Vector3d{5.0, -4.0, 2.0});
+    ptrOptimizer->optimize();
+
+    std::cout << "------- BFGS method -----" << std::endl;
+    ptrOptimizer.reset(new OptimizerBFGS(convex3d_function, Eigen::Vector3d{1.0, 1.0, 1.0}));
+    ptrOptimizer->optimize();
+    ptrOptimizer->setInitialGuess(Eigen::Vector3d{-2.0, 3.0, 1.0});
+    ptrOptimizer->optimize();
+
+    std::cout << "------- Gradient Descent -----" << std::endl;
+    ptrOptimizer.reset(new OptimizerDescent(convex3d_function, Eigen::Vector3d{1.0, 1.0, 1.0}));
+    ptrOptimizer->optimize();
+    ptrOptimizer->setInitialGuess(Eigen::Vector3d{-2.0, 3.0, 1.0});
+    ptrOptimizer->optimize();
+
+    // ----- 2D cost function (e.g. McCormick) -----
+    std::cout << "======= CostFunctionMcCormick (2D) =======" << std::endl;
     //CostFunctionQuadratic myfunction;
     //CostFunctionHimmelblau myfunction;
     CostFunctionMcCormick myfunction;
     //CostFunctionRosenbrock myfunction;
 
-    //OptimizerBase myOptimizerBase(myfunction, Eigen::Vector2d{2.0,1.0}) ;
-    //OptimizerDescent myOptimizerDescent(myfunction, Eigen::Vector2d{2.0,1.0}) ;
-    //OptimizerNewton myOptimizerNewton(myfunction, Eigen::Vector2d{2.1,1.0}) ;
+    std::cout << "------- Newton method -----" << std::endl;
+    ptrOptimizer.reset(new OptimizerNewton(myfunction, Eigen::Vector2d{2.1, 1.0}));
 
-    std::cout <<"------- Newton method -----" << std::endl;
+    ptrOptimizer->optimize();
 
-    std::unique_ptr<OptimizerBase> ptrOptimizer{new OptimizerNewton{myfunction, Eigen::Vector2d{2.1,1.0}}};
+    ptrOptimizer->setInitialGuess(Eigen::Vector2d{-4,5});
+    ptrOptimizer->optimize();
+
+    ptrOptimizer->setInitialGuess(Eigen::Vector2d{-1,-5});
+    ptrOptimizer->optimize();
+
+    ptrOptimizer->setInitialGuess(Eigen::Vector2d{6, -3});
+    ptrOptimizer->optimize();
+
+    std::cout << "------- BFGS method -----" << std::endl;
+    ptrOptimizer.reset(new OptimizerBFGS(myfunction, Eigen::Vector2d{-1.2, 1.0}));
 
     ptrOptimizer->optimize();
 
@@ -36,31 +74,8 @@ int main () {
     ptrOptimizer->setInitialGuess(Eigen::Vector2d{6,-3});
     ptrOptimizer->optimize();
 
-    std::cout <<"------- BFGS method -----" << std::endl;
-    //OptimizerDescent myOptimizerDescent(myfunction, Eigen::Vector2d{2.0,1.0}) ;
-    //ptrOptimizer = &myOptimizerDescent;
-
-    // OptimizerBFGS myOPtimizerBFGS(myfunction, Eigen::Vector2d{-1.2,1.0}) ;
-    // ptrOptimizer = &myOPtimizerBFGS;
-
-    ptrOptimizer.reset(new OptimizerBFGS(myfunction, Eigen::Vector2d{-1.2,1.0})); //= std::make_unique<OptimizerBFGS>(myfunction, Eigen::Vector2d{-1.2,1.0});
-
-    ptrOptimizer->optimize();
-
-    ptrOptimizer->setInitialGuess(Eigen::Vector2d{-4,5});
-    ptrOptimizer->optimize();
-
-    ptrOptimizer->setInitialGuess(Eigen::Vector2d{-1,-5});
-    ptrOptimizer->optimize();
-
-    ptrOptimizer->setInitialGuess(Eigen::Vector2d{6,-3});
-    ptrOptimizer->optimize();
-
-    std::cout <<"------- Gradient Descent Method -----" << std::endl;
-    // OptimizerDescent myOptimizerDescent(myfunction, Eigen::Vector2d{2.0,1.0}) ;
-    // ptrOptimizer = &myOptimizerDescent;
-
-    ptrOptimizer.reset(new OptimizerDescent(myfunction, Eigen::Vector2d{2.0,1.0}) ); //= std::make_unique<OptimizerDescent>(myfunction, Eigen::Vector2d{2.0,1.0}) ;
+    std::cout << "------- Gradient Descent Method -----" << std::endl;
+    ptrOptimizer.reset(new OptimizerDescent(myfunction, Eigen::Vector2d{2.0, 1.0}));
 
     ptrOptimizer->optimize();
 
